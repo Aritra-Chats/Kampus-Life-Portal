6b2f89020328c111d5a7877488c9814a1624dd24
Aritra-Chats <aritrathegamer05@gmail.com>
2026-02-16 22:21:45 +0530

    feat: add administration list model and controller with CRUD operations

--- backend/routes/functions.js
+++ backend/routes/functions.js
@@ -23,21 +23,30 @@ const {
     deleteAnnouncement 
 } = require('../controllers/adminController.js')
 
+const { 
+    getAdministrationList, 
+    postAdministrationData, 
+    deleteAdministrationData 
+} = require('../controllers/administrationListController.js')
+
 //get requests
 router.get('/TeacherList', getTeacherList);
 router.get('/StudentList', getStudentList);
+router.get('/AdministrationList', getAdministrationList);
 router.get('/Routine', getRoutine);
-router.get('/Announcement', getAnnouncements)
+router.get('/Announcement', getAnnouncements);
 
 //post requests
 router.post('/TeacherList', postTeacherData);
 router.post('/StudentList', postStudentData);
+router.post('/AdministrationList', postAdministrationData);
 router.post('/Routine', postRoutine);
 router.post('/Announcement', postAnnouncement);
 
 //delete requests
 router.delete('/TeacherList/:id', deleteTeacherData);
 router.delete('/StudentList/:id', deleteStudentData);
+router.delete('/AdministrationList/:id', deleteAdministrationData);
 router.delete('/Routine/:id', deleteSpecificRoutine);
 router.delete('/Announcement/:id', deleteAnnouncement);
 
