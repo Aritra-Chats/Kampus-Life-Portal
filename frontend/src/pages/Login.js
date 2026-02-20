c02f82bfdad80d75ab94847f5852a19abaea66c1
Aritra-Chats <aritrathegamer05@gmail.com>
2026-02-20 16:00:57 +0530

    refactor: update background image class names and styles across components

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
 
   return (
     <div className="Login">
-      <img className="Background" src="/images/bg.gif" alt="Background"/>
+      <img className="page-bg-image" src="/images/bg.gif" alt="Background"/>
       <GlassSurface className='Panel' width={'500px'} height={'700px'} borderRadius={10} opacity={0.5} blur={5} >
         <LoginForm />
       </GlassSurface>
